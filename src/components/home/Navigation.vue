d8d2fc0e28ccc81a97b10015b23040bd6370c092
Arif <marfsahin@gmail.com>
2021-10-31 16:24:58 +0300

    add title toolbar (navigation).
    -currently there is no navigation functionality but may be added later on this component.

---
+++ src/components/home/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+    absolute
+    elevation="24"
+    :src="require('@/assets/images/navigation.png')"
+    scroll-target="#scrolling-techniques-5"
+    scroll-threshold="500"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(206,206,206,.5), rgba(129,129,129,.8)"
+      ></v-img>
+    </template>
+    <v-spacer />
+    <v-toolbar-title><h1>Movie Listing</h1></v-toolbar-title>
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
