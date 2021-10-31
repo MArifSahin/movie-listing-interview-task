2389cb612076265b5c85b7f25a152e5b106b5297
Arif <marfsahin@gmail.com>
2021-10-31 16:29:26 +0300

    refactor app page for showing home components properly.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app app dark>
+    <navigation></navigation>
+    <search :set-movies="setMovies"></search>
+    <movie-list :movies="movies"></movie-list>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { ref } from '@vue/composition-api';
+import Navigation from './components/home/Navigation.vue';
+import Search from './components/home/Search.vue';
+import MovieList from './components/movie/MovieList.vue';
+export default {
+  components: { Navigation, Search, MovieList },
+  setup() {
+    const movies = ref([]);
 
-#nav {
-  padding: 30px;
-}
+    function setMovies(value) {
+      movies.value = value;
+      console.log(movies.value);
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    return { movies, setMovies };
+  },
+};
+</script>
